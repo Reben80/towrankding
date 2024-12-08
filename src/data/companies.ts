8ffda35f3701d6850b9056715c1c77140d3eb1d1
Rebin Muhammad <41596499+Reben80@users.noreply.github.com>
2024-12-08 14:15:58 -0500

    Update companies.ts

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -2,73 +2,38 @@ import { CompanyData } from '../types';
 
 export const towingCompanies: CompanyData[] = [
   {
+    tradeName: "Auburn Towing, Inc.",
+    distanceScore: 0.0,
+    ceiScore: 0.372843890804804,
+    mhiScore: 0.001012099,
+    englishScore: 0.7990100668951037
+  },
+  {
     tradeName: "Big Tow",
-    mhiScore: 0.951004517,
-    distanceScore: 0.986339082,
-    ceiScore: 0.7727869977893904,
-    englishScore: 0.9091886316953166
-  },
-  {
-    tradeName: "Auburn Towing",
-    mhiScore: 0.9989879006940534,
-    distanceScore: 1,
-    ceiScore: 0.6271561091951959,
-    englishScore: 0.793919319
-  },
-  {
-    tradeName: "Miller's Towing",
-    mhiScore: 1,
-    distanceScore: 0.6864761729631865,
-    ceiScore: 0.8008869067436779,
-    englishScore: 0.5941252442800821
-  },
-  {
-    tradeName: "Henry's Wrecker",
-    mhiScore: 0.9458031164713611,
-    distanceScore: 0.5088799132123464,
-    ceiScore: 1,
-    englishScore: 0.7830714272026248
-  },
-  {
-    tradeName: "Ricky's Towing",
-    mhiScore: 0.804229913,
-    distanceScore: 0.597853937,
-    ceiScore: 0.877006616,
-    englishScore: 0.9166025621649975
-  },
-  {
-    tradeName: "Speedy Towing",
-    mhiScore: 0.7628295861421327,
-    distanceScore: 0.6306360588640301,
-    ceiScore: 0.7302408340695714,
-    englishScore: 1
-  },
-  {
-    tradeName: "Authorized Towing",
-    mhiScore: 0.3727376500538821,
-    distanceScore: 0.6791099909148105,
-    ceiScore: 0.3140850232657398,
-    englishScore: 0.9598624151518167
-  },
-  {
-    tradeName: "EC",
-    mhiScore: 0.594433969,
-    distanceScore: 0,
-    ceiScore: 0.8065016831380212,
-    englishScore: 0.8881932688255061
-  },
-  {
-    tradeName: "Drive Towing",
-    mhiScore: 0.32899102191719803,
-    distanceScore: 0.5718624099766235,
-    ceiScore: 0,
-    englishScore: 0
+    distanceScore: 0.0136609180238121,
+    ceiScore: 0.2272130022106096,
+    mhiScore: 0.0489954826698509,
+    englishScore: 0.6038927577884434
   },
   {
     tradeName: "Prestige",
-    mhiScore: 0,
-    distanceScore: 0.7927586394942002,
-    ceiScore: 0.037770022,
-    englishScore: 0.539438545
+    distanceScore: 0.207241360505,
+    ceiScore: 0.9622301569999998,
+    mhiScore: 1.0,
+    englishScore: 0.9085901971970425
+  },
+  {
+    tradeName: "Millers's Towing & Recovery, LLC",
+    distanceScore: 0.3135243246016574,
+    ceiScore: 0.199113333,
+    mhiScore: 0.0,
+    englishScore: 0.317731798893114
+  },
+  {
+    tradeName: "Authorized Towing",
+    distanceScore: 0.3208900000164512,
+    ceiScore: 0.6859153121503,
+    mhiScore: 0.6272624998472839,
+    englishScore: 0.6118654894651725
   }
 ];
